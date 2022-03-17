e6834b1c7353312825ea84cc77f29e5cae87120f
Vladimir_Kirichenko <frozenfish@internet.ru>
2022-03-17 11:43:20 +0300

    убран лишний закоментированный код в urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -19,29 +19,3 @@ router.register(
 urlpatterns = [
     path('v1/', include(router.urls)),
 ]
-
-
-'''
-from django.urls import path, include
-
-from rest_framework import routers
-
-from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
-
-app_name = 'api'
-
-router = routers.DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('groups', GroupViewSet)
-router.register(
-    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments'
-)
-router.register('follow', FollowViewSet, basename='follow')
-
-
-urlpatterns = [
-    path('v1/', include(router.urls)),
-    path('v1/', include('djoser.urls')),
-    path('v1/', include('djoser.urls.jwt')),
-]
-'''
