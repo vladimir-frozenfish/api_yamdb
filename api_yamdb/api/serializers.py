c0d770535a29ed70a9ec5c2209832179b0439a9b
Vladimir Vasilev <vasilevvladv@yandex.ru>
2022-03-19 13:03:35 +0400

    Created validate_username fuction

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,9 @@
+import re
+
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-
 from reviews.models import Comment, Review, User
+
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -11,9 +13,13 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         
     def validate_username(self, value):
-        
-        pass
-
+        if value is None:
+            raise serializers.ValidationError('Вы не указали username')
+        elif not re.search(r'^\w+$', value):
+            raise serializers.ValidationError('Username должен состоять из букв, цифр и символа подчеркивания')
+        elif value == 'me':
+            raise serializers.ValidationError('Пользователь "me" запрещен. Имя зарезервировано.')
+        return value
 
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
