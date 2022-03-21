21ac0743fee021c73077af208b72faa80b17d1b5
Vladimir_Kirichenko <frozenfish@internet.ru>
2022-03-21 11:42:04 +0300

    Тесты по комментариям проходят.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -189,9 +189,20 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly,
+    ]
 
     def get_queryset(self):
         review_id = self.kwargs.get("review_id")
         review = get_object_or_404(Review, id=review_id)
         new_queryset = review.comments.all()
         return new_queryset
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get("review_id")
+        review = get_object_or_404(Review, id=review_id)
+        serializer.save(author=self.request.user, review=review)
+
+
